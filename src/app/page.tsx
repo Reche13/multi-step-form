a42a595a102e78d69d90c91a50d5555873e31618
Reche Soares <rechesoares13@gmail.com>
2025-05-04 17:17:10 +0530

    add basic stepper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+"use client";
+import Stepper from "@/components/Stepper";
+
 export default function Home() {
-  return <div className="">hello world</div>;
+  return (
+    <div className="w-full px-8">
+      <div className="w-full max-w-[1280px] mx-auto pt-20">
+        <Stepper />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/Stepper/index.tsx
@@ -0,0 +1,136 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const STEPPER_OPTIONS = [
+  {
+    label: "Upload Resume",
+    Component: () => <div className="">Upload Resume</div>,
+  },
+  {
+    label: "Basic Information",
+    Component: () => <div className="">Basic Information</div>,
+  },
+  {
+    label: "Skill Set",
+    Component: () => <div className="">Skill Set</div>,
+  },
+  {
+    label: "Education",
+    Component: () => <div className="">Education</div>,
+  },
+  {
+    label: "Summary",
+    Component: () => <div className="">Summary</div>,
+  },
+  {
+    label: "Completed",
+    Component: () => <div className="">Completed</div>,
+  },
+];
+
+const Stepper = ({ steps = STEPPER_OPTIONS }) => {
+  const [currentStep, setCurrentStep] = useState(0);
+  const [margins, setMargins] = useState({
+    left: 0,
+    right: 0,
+  });
+
+  const stepRef = useRef<(HTMLDivElement | null)[]>([]);
+
+  const handleNext = () => {
+    setCurrentStep((prevStep) => {
+      if (prevStep === steps.length) {
+        return prevStep;
+      } else {
+        return prevStep + 1;
+      }
+    });
+  };
+
+  useEffect(() => {
+    const first = stepRef.current[0];
+    const last = stepRef.current[steps.length - 1];
+    if (first && last) {
+      setMargins({
+        left: first.offsetWidth / 2,
+        right: last.offsetWidth / 2,
+      });
+    }
+  }, [stepRef.current, steps.length]);
+
+  const calculateProgress = () => {
+    return Math.min(1, currentStep / (steps.length - 1)) * 100;
+  };
+
+  return (
+    <>
+      <div className={`grid grid-cols-6 relative`}>
+        {steps.map((step, index) => (
+          <div
+            ref={(el) => {
+              stepRef.current[index] = el;
+            }}
+            key={step.label}
+            className="flex flex-col items-center gap-4"
+          >
+            <div
+              className={`w-8 h-8 flex items-center justify-center border-2 rounded-full ${
+                index === currentStep ? "border-primary" : "border-[#D1D5DB]"
+              } ${
+                index < currentStep ? "bg-primary border-primary" : "bg-white"
+              }`}
+            >
+              {index === currentStep && (
+                <div className="w-2.5 h-2.5 rounded-full bg-primary" />
+              )}
+              {index === currentStep + 1 && (
+                <div className="w-2.5 h-2.5 rounded-full bg-[#D1D5DB]" />
+              )}
+              {index < currentStep && (
+                <svg
+                  width="14"
+                  height="11"
+                  viewBox="0 0 14 11"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M1 6L4.33333 9.33333L12.6667 1"
+                    stroke="white"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              )}
+            </div>
+            <div className="text-sm text-[#484848] font-medium">
+              {step.label}
+            </div>
+          </div>
+        ))}
+
+        {margins.left !== 0 && margins.right !== 0 && (
+          <div
+            style={{
+              width: `calc(100% - (${margins.left + margins.right}px))`,
+              marginLeft: `${margins.left}px`,
+              marginRight: `${margins.right}px`,
+            }}
+            className="absolute w-0 z-[-1] top-[15px] left-0 h-0.5 bg-[#D1D5DB]"
+          >
+            <div
+              className="bg-primary h-full"
+              style={{ width: `${calculateProgress()}%` }}
+            />
+          </div>
+        )}
+      </div>
+
+      <button onClick={handleNext} className="border p-4 cursor-pointer">
+        {currentStep === steps.length ? "Finish" : "Next"}
+      </button>
+    </>
+  );
+};
+
+export default Stepper;
